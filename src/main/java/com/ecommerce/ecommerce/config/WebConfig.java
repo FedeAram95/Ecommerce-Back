e7653a0342ad4e95818d066e302d174624babc1c
Federico <federico.aramburu@sociallearning.io>
2025-05-22 20:15:40 -0300

    Se desactiva Swagger y spring security

--- src/main/java/com/ecommerce/ecommerce/config/WebConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/WebConfig.java
@@ -2,23 +2,21 @@ package com.ecommerce.ecommerce.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://tudominio.com") // orígenes válidos
                 .allowedMethods("*")
-                .maxAge(3600L)
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600L);
     }
 
     @Override
